573a827cbef41c3bb469241e7cf6242c45bd6f5f
Stefan Scherfke <stefan@sofa-rockers.org>
2020-09-16 21:53:05 +0200

    Add nox and linting

---
+++ noxfile.py
@@ -0,0 +1,64 @@
+import glob
+
+import nox
+
+
+LOCATIONS = [
+    # "docs/conf.py",
+    "noxfile.py",
+    "setup.py",
+    "src/",
+    "tests/",
+]
+
+
+@nox.session
+def build(session):
+    session.install("pep517")
+    session.run("rm", "-rf", "build", "dist")
+    session.run("python", "-m", "pep517.build", "--source", "--binary", ".")
+
+
+@nox.session(python=["3.7", "3.8", "3.9"])
+@nox.parametrize("pkg_format", [".", "tar.gz", "whl"], [".", "src", "whl"])
+def test(session, pkg_format):
+    if pkg_format == ".":
+        src = "."
+    else:
+        pkgs = glob.glob(f"dist/*.{pkg_format}")
+        if len(pkgs) == 0:
+            session.log('Package not found, running "build" ...')
+            build(session)
+            pkgs = glob.glob(f"dist/*.{pkg_format}")
+        if len(pkgs) != 1:
+            session.error(f"Expected exactly 1 file: {', '.join(pkgs)}")
+        src = pkgs[0]
+
+    session.install(f"{src}[test]")
+
+    args = session.posargs or ["tests"]
+    # session.run("pytest", *args)
+    session.run("coverage", "run", "-m", "pytest", *args)
+
+
+@nox.session(name="coverage-report")
+def coverage_report(session):
+    session.install(".[test]")
+    session.run("coverage", "combine")
+    session.run("coverage", "xml")
+    session.run("coverage", "report")
+
+
+@nox.session
+def lint(session):
+    session.install(".[lint]")
+
+    args = session.posargs or LOCATIONS
+    session.run("flake8", *args)
+
+@nox.session
+def mypy(session):
+    session.install(".[mypy]")
+
+    args = session.posargs or ["--ignore-missing-imports"] + LOCATIONS
+    session.run("mypy", *args)
--- src/typed_settings/_click.py
+++ src/typed_settings/_click.py
@@ -16,25 +16,30 @@ def click_options(settings_cls, appname, config_files):
         """
 
         def new_func(*args, **kwargs):
-            return f(click.get_current_context().obj['settings'], *args, **kwargs)
+            return f(
+                click.get_current_context().obj["settings"], *args, **kwargs
+            )
 
         return update_wrapper(new_func, f)
 
     def cb(c, p, v):
         if c.obj is None:
             c.obj = {}
-        if 'settings' not in c.obj:
-            c.obj['settings'] = {}#typed_settings.load_settings(cls=settings_cls)
-        c.obj['settings'][p.name] = v
+        if "settings" not in c.obj:
+            c.obj[
+                "settings"
+            ] = {}  # typed_settings.load_settings(cls=settings_cls)
+        c.obj["settings"][p.name] = v
         return v
 
     def wrap(f):
         f = click.option(
-            '--spam', callback=cb, expose_value=False, is_eager=True
+            "--spam", callback=cb, expose_value=False, is_eager=True
         )(f)
         f = click.option(
-            '--eggs', callback=cb, expose_value=False, is_eager=True
+            "--eggs", callback=cb, expose_value=False, is_eager=True
         )(f)
         f = pass_settings(f)
         return f
+
     return wrap
