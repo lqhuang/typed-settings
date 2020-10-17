67381bf96c8124e78e3b0379db18a5cf2c92835a
Stefan Scherfke <stefan@sofa-rockers.org>
2020-10-17 22:44:48 +0200

    Move attrs helpers into their own module

--- src/typed_settings/__init__.py
+++ src/typed_settings/__init__.py
@@ -1,11 +1,9 @@
 """
 Typed settings
 """
-from functools import partial
 from typing import Any, List
 
-import attr
-
+from ._attrs import option, secret, settings
 from ._core import load_settings, update_settings
 
 
@@ -35,40 +33,3 @@ def __getattr__(name: str) -> Any:
 
 def __dir__() -> List[str]:
     return __all__
-
-
-class _SecretRepr:
-    def __call__(self, _v):
-        return "***"
-
-    def __repr__(self):
-        return "***"
-
-
-settings = attr.frozen
-"""An alias to :func:`attr.frozen()`"""
-
-# settings = partial(attr.frozen, field_transformer=attr.auto_convert)
-option = partial(attr.field)
-"""An alias to :func:`attr.field()`"""
-
-secret = partial(attr.field, repr=_SecretRepr())
-"""
-An alias to :func:`attr.field()`.
-
-When printing a settings instances, secret settings will represented with `***`
-istead of their actual value.
-
-Example:
-
-  .. code-block:: python
-
-     >>> from typed_settings import settings, secret
-     >>>
-     >>> @settings
-     ... class Settings:
-     ...     password: str = secret()
-     ...
-     >>> Settings(password="1234")
-     Settings(password=***)
-"""
