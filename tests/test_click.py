43eb5f737baff621a51c1397808484e00ff702ae
Stefan Scherfke <stefan@sofa-rockers.org>
2020-11-06 22:46:33 +0100

    Refactor click tests to make them more modular

--- tests/test_click.py
+++ tests/test_click.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from pathlib import Path
+from typing import Any, Callable, List
 
 import click
 import click.testing
@@ -14,176 +15,255 @@ from typed_settings import (
 )
 
 
+def make_cli(settings_cls: type) -> Callable[..., Any]:
+    @pytest.fixture
+    def cli(self, tmp_path):
+        """
+        Creates a click command for ``Settings`` and returns a functions that
+        invokes a click test runner with the passed arguments.
+
+        The result object will habe a ``settings`` attribute that holds the
+        generated ``Settings`` instance for verification.
+
+        """
+
+        class Runner(click.testing.CliRunner):
+            settings: object
+
+            def invoke(self, *args, **kwargs):
+                result = super().invoke(*args, **kwargs)
+                try:
+                    result.settings = self.settings
+                except AttributeError:
+                    result.settings = None
+                return result
+
+        runner = Runner()
+
+        @click.group(invoke_without_command=True)
+        @click_options(
+            settings_cls, "test", [tmp_path.joinpath("settings.toml")]
+        )
+        def cli(settings):
+            runner.settings = settings
+
+        def run(*args, **kwargs):
+            return runner.invoke(cli, args, **kwargs)
+
+        return run
+
+    return cli
+
+
+class ClickTestBase:
+
+    _help: List[str] = []
+    _defaults: Any = None
+    _options: List[str] = []
+    _values: Any = None
+
+    def test_help(self, cli):
+        result = cli("--help")
+
+        # fmt: off
+        assert result.output.splitlines()[:-1] == [
+            "Usage: cli [OPTIONS] COMMAND [ARGS]...",
+            "",
+            "Options:",
+        ] + self._help
+        assert result.exit_code == 0
+        # fmt: on
+
+    def test_defaults(self, cli):
+        result = cli()
+        assert result.output == ""
+        assert result.exit_code == 0
+        assert result.settings == self._defaults
+
+    def test_options(self, cli):
+        result = cli(*self._options)
+        assert result.output == ""
+        assert result.exit_code == 0
+        assert result.settings == self._values
+
+
+class TestClickBool(ClickTestBase):
+    @settings
+    class S:
+        a: bool
+        b: bool = True
+        c: bool = False
+
+    cli = make_cli(S)
+
+    _help = [
+        "  --a / --no-a  [default: False]",
+        "  --b / --no-b  [default: True]",
+        "  --c / --no-c  [default: False]",
+    ]
+    _defaults = S(False, True, False)
+    _options = ["--no-a", "--no-b", "--c"]
+    _values = S(False, False, True)
+
+
+class TestIntFloatStr(ClickTestBase):
+    @settings
+    class S:
+        a: str = option(default="spam")
+        b: str = secret(default="spam")
+        c: int = 0
+        d: float = 0
+
+    cli = make_cli(S)
+
+    _help = [
+        "  --a TEXT     [default: spam]",
+        "  --b TEXT     [default: spam]",
+        "  --c INTEGER  [default: 0]",
+        "  --d FLOAT    [default: 0]",
+    ]
+    _defaults = S()  # type: ignore
+    _options = ["--a=eggs", "--b=pwd", "--c=3", "--d=3.1"]
+    _values = S(a="eggs", b="pwd", c=3, d=3.1)
+
+
+# class TestMandatory(ClickTestBase):
+#
+#     @settings
+#     class S:
+#         a: str
+#
+#     cli = make_cli(S)
+#
+#     _help = (
+#         "  --a TEXT\n"
+#         "  --b TEXT\n"
+#     )
+#     _defaults = S()
+#     _options = ["--a=eggs1", "--b=eggs2"]
+#     _values = S(a="eggs1", b="eggs2")
+
+
+# class TestDateTime(ClickTestBase):
+#     pass
+
+
 class LeEnum(Enum):
     spam = "le spam"
     eggs = "Le eggs"
 
 
-@settings
-class Nested:
-    a: str = "nested"
-    b: int = option(default=0, converter=int)  # type: ignore
+class TestEnum(ClickTestBase):
+    @settings
+    class S:
+        a: LeEnum = LeEnum.spam
+
+    cli = make_cli(S)
+
+    _help = ["  --a [spam|eggs]  [default: spam]"]
+    _defaults = S(LeEnum.spam)
+    _options = ["--a=eggs"]
+    _values = S(LeEnum.eggs)
 
 
-@settings(kw_only=True)
-class Settings:
-    a: str
-    b: str = secret()
-    c: str = option(default="spam")
-    d: int = 0
-    e: float = 0
-    f: bool = False
-    g: bool = True
-    h: LeEnum = LeEnum.spam
-    p: Path = Path("/")
-    n: Nested = Nested()  # type: ignore
-    long_name: str = option(default="val")  # type: ignore
+class TestPath(ClickTestBase):
+    @settings
+    class S:
+        a: Path = Path("/")
+
+    cli = make_cli(S)
+
+    _help = ["  --a PATH  [default: /]"]
+    _defaults = S()
+    _options = ["--a=/spam"]
+    _values = S(Path("/spam"))
 
 
-@pytest.fixture
-def cli(tmp_path):
+class TestNested(ClickTestBase):
+    @settings
+    class S:
+        @settings
+        class Nested:
+            a: str = "nested"
+            b: int = option(default=0, converter=int)  # type: ignore
+
+        n: Nested = Nested()  # type: ignore
+
+    cli = make_cli(S)
+
+    _help = [
+        "  --n-a TEXT     [default: nested]",
+        "  --n-b INTEGER  [default: 0]",
+    ]
+    _defaults = S()
+    _options = ["--n-a=eggs", "--n-b=3"]
+    _values = S(S.Nested("eggs", 3))
+
+
+def test_long_name():
     """
-    Creates a click command for ``Settings`` and returns a functions that
-    invokes a click test runner with the passed arguments.
-
-    The result object will habe a ``settings`` attribute that holds the
-    generated ``Settings`` instance for verification.
-
+    Underscores in option names are replaces with "-" in Click options.
     """
 
-    class Runner(click.testing.CliRunner):
-        settings: object
+    @settings
+    class S:
+        long_name: str = "val"
 
-        def invoke(self, *args, **kwargs):
-            result = super().invoke(*args, **kwargs)
-            try:
-                result.settings = self.settings
-            except AttributeError:
-                result.settings = None
-            return result
+    @click.command()
+    @click_options(S, "test")
+    def cli(settings):
+        pass
 
-    runner = Runner()
+    runner = click.testing.CliRunner()
+    result = runner.invoke(cli, ["--help"])
+    assert result.output == (
+        "Usage: cli [OPTIONS]\n"
+        "\n"
+        "Options:\n"
+        "  --long-name TEXT  [default: val]\n"
+        "  --help            Show this message and exit.\n"
+    )
+    assert result.exit_code == 0
 
-    @click.group(invoke_without_command=True)
+
+def test_click_default_from_settings(monkeypatch, tmp_path):
+    """
+    If a setting is set in a config file, that value is being used as default
+    for click options - *not* the default defined in the Settings class.
+    """
+
+    tmp_path.joinpath("settings.toml").write_text('[test]\na = "x"\n')
+    spath = tmp_path.joinpath("settings2.toml")
+    print(spath)
+    spath.write_text('[test]\nb = "y"\n')
+    monkeypatch.setenv("TEST_SETTINGS", str(spath))
+    monkeypatch.setenv("TEST_C", "z")
+
+    @settings
+    class Settings:
+        a: str
+        b: str
+        c: str
+        d: str
+
+    @click.command()
     @click_options(Settings, "test", [tmp_path.joinpath("settings.toml")])
     def cli(settings):
-        runner.settings = settings
+        print(settings)
 
-    def run(*args, **kwargs):
-        return runner.invoke(cli, args, **kwargs)
-
-    return run
-
-
-class TestClickOptions:
-    """Tests for click_options()."""
-
-    def test_click_options(self, cli):
-        """Make sure click options of various types work."""
-
-        result = cli(
-            "--a=eggs1",
-            "--b=eggs2",
-            "--c=eggs3",
-            "--d=3",
-            "--e=3.1",
-            "--f",
-            "--no-g",
-            "--h=eggs",
-            "--p=/spam",
-            "--n-a=eggs4",
-            "--n-b=3",
-        )
-        assert result.output == ""
-        assert result.exit_code == 0
-        assert result.settings == Settings(
-            a="eggs1",
-            b="eggs2",
-            c="eggs3",
-            d=3,
-            e=3.1,
-            f=True,
-            g=False,
-            h=LeEnum.eggs,
-            p=Path("/spam"),
-            n=Nested("eggs4", 3),
-        )
-
-    def test_click_defaults(self, cli):
-        """
-        Most types do not accept "None" as value, so most options need to have
-        a default value.
-        """
-        result = cli()
-        assert result.output == ""
-        assert result.exit_code == 0
-        assert result.settings == Settings(
-            a=None,
-            b=None,
-        )
-
-    def test_help(self, cli):
-        """All options get a proper help string."""
-        result = cli("--help")
-        assert result.output == (
-            "Usage: cli [OPTIONS] COMMAND [ARGS]...\n"
-            "\n"
-            "Options:\n"
-            "  --a TEXT\n"
-            "  --b TEXT\n"
-            "  --c TEXT          [default: spam]\n"
-            "  --d INTEGER       [default: 0]\n"
-            "  --e FLOAT         [default: 0]\n"
-            "  --f / --no-f      [default: False]\n"
-            "  --g / --no-g      [default: True]\n"
-            "  --h [spam|eggs]   [default: spam]\n"
-            "  --p PATH          [default: /]\n"
-            "  --n-a TEXT        [default: nested]\n"
-            "  --n-b INTEGER     [default: 0]\n"
-            "  --long-name TEXT  [default: val]\n"
-            "  --help            Show this message and exit.\n"
-        )
-        assert result.exit_code == 0
-
-    def test_click_default_from_settings(self, monkeypatch, tmp_path):
-        """
-        If a setting is set in a config file, that value is being used as
-        default for click options - *not* the default defined in the Settings
-        class.
-        """
-
-        tmp_path.joinpath("settings.toml").write_text('[test]\na = "x"\n')
-        spath = tmp_path.joinpath("settings2.toml")
-        print(spath)
-        spath.write_text('[test]\nb = "y"\n')
-        monkeypatch.setenv("TEST_SETTINGS", str(spath))
-        monkeypatch.setenv("TEST_C", "z")
-
-        @settings
-        class Settings:
-            a: str
-            b: str
-            c: str
-            d: str
-
-        @click.command()
-        @click_options(Settings, "test", [tmp_path.joinpath("settings.toml")])
-        def cli(settings):
-            print(settings)
-
-        runner = click.testing.CliRunner()
-        result = runner.invoke(cli, ["--help"])
-        assert result.output == (
-            "Usage: cli [OPTIONS]\n"
-            "\n"
-            "Options:\n"
-            "  --a TEXT  [default: x]\n"
-            "  --b TEXT  [default: y]\n"
-            "  --c TEXT  [default: z]\n"
-            "  --d TEXT\n"
-            "  --help    Show this message and exit.\n"
-        )
-        assert result.exit_code == 0
+    runner = click.testing.CliRunner()
+    result = runner.invoke(cli, ["--help"])
+    assert result.output == (
+        "Usage: cli [OPTIONS]\n"
+        "\n"
+        "Options:\n"
+        "  --a TEXT  [default: x]\n"
+        "  --b TEXT  [default: y]\n"
+        "  --c TEXT  [default: z]\n"
+        "  --d TEXT\n"
+        "  --help    Show this message and exit.\n"
+    )
+    assert result.exit_code == 0
 
 
 class TestPassSettings:
