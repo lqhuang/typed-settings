adbbdd029a86a6d13463bf9110c6010e2cd173b1
Stefan Scherfke <stefan@sofa-rockers.org>
2022-09-01 21:39:07 +0200

    Include (c)attrs versions in build matrix
    It may be a good idea to test against the oldest and latest
    versions of attrs and cattrs.
    
    To avoid duplication, we parse their min required version from
    pyproject.toml

--- noxfile.py
+++ noxfile.py
@@ -1,19 +1,42 @@
 import glob
+import pathlib
+
+
+try:
+    import tomllib
+except ImportError:
+    import tomli as tomllib  # type: ignore[no-redef]
 
 import nox
+from packaging.requirements import Requirement
 
 
+PROJECT_DIR = pathlib.Path(__file__).parent
 LINT_PATHS = [
-    # "docs/conf.py",
-    "noxfile.py",
-    "setup.py",
-    "src/",
-    "tests/",
+    [
+        "noxfile.py",
+        "src/",
+        "tests/",
+    ],
+    [
+        "docs/conf.py",
+    ],
+    [
+        "docs/conftest.py",
+    ],
+    [
+        "docs/examples/",
+    ],
 ]
+# Dependencies for which to test against multiple versions
+DEPS_MATRIX = {
+    "attrs",
+    "cattrs",
+}
 
 
 @nox.session
-def build(session):
+def build(session: nox.Session) -> None:
     session.install("build", "check-wheel-contents")
     session.run("rm", "-rf", "build", "dist", external=True)
     session.run("python", "-m", "build", "--sdist", "--wheel", ".")
@@ -21,8 +44,15 @@ def build(session):
 
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+@nox.parametrize(
+    "deps_min_version",
+    [True, False],
+    ["min_deps_version", "latest_deps_version"],
+)
 @nox.parametrize("pkg_format", [".", "tar.gz", "whl"], [".", "src", "whl"])
-def test(session, pkg_format):
+def test(
+    session: nox.Session, deps_min_version: bool, pkg_format: str
+) -> None:
     if pkg_format == ".":
         src = "."
     else:
@@ -35,7 +65,25 @@ def test(session, pkg_format):
             session.error(f"Expected exactly 1 file: {', '.join(pkgs)}")
         src = pkgs[0]
 
-    session.install(f"{src}[test]")
+    # If testing against the minium versions of our dependencies,
+    # extract their minium version from pyproject.toml and pin
+    # these versions.
+    install_deps = []
+    if deps_min_version:
+        pyproject = tomllib.loads(
+            PROJECT_DIR.joinpath("pyproject.toml").read_text()
+        )
+        deps = pyproject["project"]["dependencies"]
+        for dep in deps:
+            req = Requirement(dep)
+            if req.name not in DEPS_MATRIX:
+                continue
+            spec = str(req.specifier)
+            assert spec.startswith(">="), spec
+            spec = spec.replace(">=", "==")
+            install_deps.append(f"{req.name}{spec}")
+
+    session.install(f"{src}[test]", *install_deps)
 
     # We have to run the tests for the doctests in "src" separately or we'll
     # get an "ImportPathMismatchError" (the "same" file is located in the
@@ -45,7 +93,7 @@ def test(session, pkg_format):
 
 
 @nox.session(name="coverage-report")
-def coverage_report(session):
+def coverage_report(session: nox.Session) -> None:
     session.install(".[test]")
     session.run("coverage", "combine")
     # Only let the "report" command fail under 100%
@@ -55,18 +103,20 @@ def coverage_report(session):
 
 
 @nox.session
-def lint(session):
+def lint(session: nox.Session) -> None:
     session.install(".[lint]")
-    session.run("flake8", *LINT_PATHS)
+    for paths in LINT_PATHS:
+        session.run("flake8", *paths)
 
 
 @nox.session
-def mypy(session):
+def mypy(session: nox.Session) -> None:
     session.install(".[lint]")
-    session.run("mypy", "--show-error-codes", *LINT_PATHS)
+    for paths in LINT_PATHS:
+        session.run("mypy", "--show-error-codes", *paths)
 
 
 @nox.session
-def safety(session):
+def safety(session: nox.Session) -> None:
     session.install(".[dev]")  # Install *everything*
     session.run("safety", "check")
