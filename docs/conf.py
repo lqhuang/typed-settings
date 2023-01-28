b2894b618ff34324b5930c88de8d1262de5c0542
Stefan Scherfke <stefan@sofa-rockers.org>
2023-01-28 20:31:32 +0100

    Add sphinx-copybutton

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
+    "sphinx_copybutton",
 ]
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
